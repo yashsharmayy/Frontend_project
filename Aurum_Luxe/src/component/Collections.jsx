2c2700e124d535c8a23799e55df3a5fe4efcd495
yash sharma <ys4468570@gmail.com>
2026-04-11 16:01:00 +0530

    feat: enhance Collections component; add hover effects and additional content for image sections

--- Aurum_Luxe/src/component/Collections.jsx
+++ Aurum_Luxe/src/component/Collections.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Collections = () => {
     return (
         <div className='w-full h-full lg:mb-30  p-10 py-20 lg:p-20 overflow-hidden  bg-cream09'>
-            <div className='flex flex-col h-full md:flex-row my-20 items-start md:items-center justify-between'>
+            <div className='flex flex-col group h-full md:flex-row my-20 items-start md:items-center justify-between'>
                 <div className='flex flex-col gap-5'>
                     <h1 className='font-cormorant text-5xl text-charcoal69'>Our Collections</h1>
                     <p className='text-gold-dark59'>Discover pieces crafted for every chapter of your story</p>
@@ -13,50 +13,82 @@ const Collections = () => {
                         <h3>View All Collections</h3>
                         <i className="fa-solid fa-arrow-right mx-1 text-sm transition-transform duration-300 ease-in-out group-hover:translate-x-1"></i>
                     </button>
+
                 </div>
             </div>
             <div className="gallery h-full w-full ">
 
-                <div className='flex h-full md:flex-row flex-col gap-8'>
-                    <div className="md:w-1/2 h-120 overflow-hidden rounded-lg">
+                <div className='flex h-full  md:flex-row flex-col gap-8'>
+                    <div className=" group md:w-1/2 h-120 relative overflow-hidden rounded-lg">
                         <img
-                            className="img-transform  "
+                            className="img-transform duration-500 group-hover:scale-110 "
                             src="https://images.unsplash.com/photo-1679156271456-d6068c543ee7?w=400&auto=format&fit=crop&q=60"
                             alt=""
                         />
+                        <div className=' absolute opacity-0  group-hover:opacity-100 transition-all duration-500 inset-0 bg-linear-to-b from-[#15020204]  to-[#151414b3]'>
+                            <div className='absolute bottom-10 left-10'>
+                                <h2 className='text-cream09 font-cormorant text-3xl my-3'>Statement</h2>
+                                <p className='text-taupe-light99'>18 pieces</p>
+                            </div>
+                        </div>
                     </div>
 
-                    <div className='w-full h-120 overflow-hidden rounded-lg '>
+                    <div className='w-full h-120 relative group overflow-hidden rounded-lg group '>
+
                         <img
-                            className="img-transform rounded-lg"
+                            className="img-transform duration-500 group-hover:scale-110 rounded-lg"
                             src="https://images.unsplash.com/photo-1747116404311-55f8d8944e83?w=400&auto=format&fit=crop&q=60"
                             alt=""
                         />
+                        <div className=' absolute opacity-0  group-hover:opacity-100 transition-all duration-500 inset-0 bg-linear-to-b from-[#15020204]  to-[#151414b3]'>
+                            <div className='absolute bottom-10 left-10'>
+                                <h2 className='text-cream09 font-cormorant text-3xl my-3'>Bridal</h2>
+                                <p className='text-taupe-light99'>24 pieces</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
                 <div className='flex my-8 h-full md:flex-row flex-col gap-8 md:justify-evenly '>
-                    <div className='w-full h-120 md:m-4 overflow-hidden rounded-lg '>
+                    <div className='w-full group relative h-120 md:m-4 overflow-hidden rounded-lg '>
                         <img
-                            className="img-transform"
+                            className="img-transform duration-500 group-hover:scale-110"
                             src="https://images.unsplash.com/photo-1588909006332-2e30f95291bc?w=400&auto=format&fit=crop&q=60"
                             alt=""
                         />
+                        <div className=' absolute opacity-0  group-hover:opacity-100 transition-all duration-500 inset-0 bg-linear-to-b from-[#15020204]  to-[#151414b3]'>
+                            <div className='absolute bottom-10 left-10'>
+                                <h2 className='text-cream09 font-cormorant text-3xl my-3'>Men's Collection</h2>
+                                <p className='text-taupe-light99'>15 pieces</p>
+                            </div>
+                        </div>
                     </div>
 
-                    <div className='md:m-4 w-full h-120 overflow-hidden rounded-lg '>
+                    <div className='md:m-4 group relative w-full h-120 overflow-hidden rounded-lg '>
                         <img
-                            className="img-transform "
+                            className="img-transform duration-500 group-hover:scale-110 "
                             src="https://plus.unsplash.com/premium_photo-1674581921333-959b929a2e0a?w=400&auto=format&fit=crop&q=60"
                             alt=""
                         />
+                        <div className=' absolute opacity-0  group-hover:opacity-100 transition-all duration-500 inset-0 bg-linear-to-b from-[#15020204]  to-[#151414b3]'>
+                            <div className='absolute bottom-10 left-10'>
+                                <h2 className='text-cream09 font-cormorant text-3xl my-3'>Heritage</h2>
+                                <p className='text-taupe-light99'>12 pieces</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className='md:m-4 w-full h-120 overflow-hidden rounded-lg '>
+                    <div className='md:m-4 group relative w-full h-120 overflow-hidden rounded-lg '>
                         <img
-                            className="img-transform "
+                            className="img-transform duration-500 group-hover:scale-110"
                             src="https://images.unsplash.com/photo-1682822749962-d4510e0467fb?w=400&auto=format&fit=crop&q=60"
                             alt=""
                         />
+                        <div className=' absolute opacity-0  group-hover:opacity-100 transition-all duration-500 inset-0 bg-linear-to-b from-[#15020204]  to-[#151414b3]'>
+                            <div className='absolute bottom-10 left-10'>
+                                <h2 className='text-cream09 font-cormorant text-3xl my-3'>Everyday Elegance</h2>
+                                <p className='text-taupe-light99'>36 pieces</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
